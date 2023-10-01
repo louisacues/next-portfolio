fc3dc3619b72fe47785e3f6a0eac78ce25055098
louisacues <cuenzalouisa@gmail.com>
2023-10-02 02:27:54 +0800

    test5

--- src/components/projects/Calculator.tsx
+++ src/components/projects/Calculator.tsx
@@ -1,44 +1,44 @@
-'use client'
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
+import * as math from 'mathjs'
 
 export default function Calculator() {
   const [displayValue, setDisplayValue] = useState<string>('')
 
+  const handleKeyPress = useCallback(
+    (event: KeyboardEvent) => {
+      const key = event.key
+      if ('0123456789.'.includes(key)) {
+        setDisplayValue((prev) => prev + key)
+      } else if (key === 'Enter' || key === '=') {
+        handleButtonClick('=')
+      } else if (['+', '-', '*', '/'].includes(key)) {
+        setDisplayValue((prev) => prev + key)
+      } else if (key === 'Backspace') {
+        handleButtonClick('C')
+      }
+    },
+    [displayValue],
+  )
+
   useEffect(() => {
-    // This will be called when the component mounts
     window.addEventListener('keydown', handleKeyPress)
 
-    // This will be called when the component unmounts
     return () => {
       window.removeEventListener('keydown', handleKeyPress)
     }
-  }, [displayValue])
-
-  const handleKeyPress = (event: KeyboardEvent) => {
-    const key = event.key
-    if ('0123456789.'.includes(key)) {
-      setDisplayValue((prev) => prev + key)
-    } else if (key === 'Enter' || key === '=') {
-      handleButtonClick('=')
-    } else if (['+', '-', '*', '/'].includes(key)) {
-      setDisplayValue((prev) => prev + key)
-    } else if (key === 'Backspace') {
-      handleButtonClick('C')
-    }
-    // ... add other key conditions if necessary
-  }
+  }, [handleKeyPress])
 
   const handleButtonClick = (value: string) => {
     if (value === '=') {
       try {
-        setDisplayValue(eval(displayValue).toString())
+        setDisplayValue(math.evaluate(displayValue).toString())
       } catch (error) {
         setDisplayValue('Error')
       }
     } else if (value === 'C') {
       setDisplayValue('')
     } else {
-      setDisplayValue(displayValue + value)
+      setDisplayValue((prev) => prev + value)
     }
   }
 
@@ -61,6 +61,7 @@ export default function Calculator() {
     '+',
     'C',
   ]
+
   return (
     <div className="mx-auto my-8 w-64 rounded border border-blue-600/20 p-4 text-center drop-shadow-2xl">
       <input
